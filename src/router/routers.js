13c4e324da48972845b0fe62390ea431cf13f56f
summersnake <33661226lll@163.com>
2019-05-18 10:52:15 +0800

    【删除】图片

--- src/router/routers.js
+++ src/router/routers.js
@@ -3,9 +3,6 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Login from '../pages/Login/index.tsx';
 
 export default class Routers extends React.Component {
-  constructor(props) {
-    super(props);
-  }
   render() {
     return (
       <BrowserRouter>
