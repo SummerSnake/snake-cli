4ff6b7a2bd989e12172014d992f9358f4093481c
summersnake <33661226lll@163.com>
2019-05-31 10:41:58 +0800

    【bug fix】HashRouter

--- src/router/routers.tsx
+++ src/router/routers.tsx
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 import Login from '../pages/Login/index';
 import BasicLayout from '../BasicLayout/index';
 
 export default class Routers extends React.Component {
   render() {
     return (
-      <BrowserRouter>
+      <HashRouter>
         <Switch>
           <Route path="/login" component={Login} />
           <Route path="/" component={BasicLayout} />
         </Switch>
-      </BrowserRouter>
+      </HashRouter>
     );
   }
 }
