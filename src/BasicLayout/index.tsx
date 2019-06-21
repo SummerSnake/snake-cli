8c2564e1b533e18ad1d7f362b5587808ff233a69
summersnake <33661226lll@163.com>
2019-06-21 14:07:53 +0800

    【优化】BasicLayout change to react hooks.

--- src/BasicLayout/index.tsx
+++ src/BasicLayout/index.tsx
@@ -1,35 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Layout } from 'antd';
-import SiderMenu from './components/SiderMenu/index';
 const { Header, Content, Sider, Footer } = Layout;
 import { Route } from 'react-router-dom';
 import routes from '@config/routes';
+import SiderMenu from './components/SiderMenu/index';
 import styles from './index.less';
 
-export default class BasicLayout extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
+export default function BasicLayout() {
     return (
-      <Layout className={styles.layoutWrap}>
-        <Sider className={styles.siderDom}>
-          <div className={styles.logo}>Snake-cli</div>
-          <SiderMenu />
-        </Sider>
+        <Layout className={styles.layoutWrap}>
+            <Sider className={styles.siderDom}>
+                <div className={styles.logo}>Snake-cli</div>
+                <SiderMenu />
+            </Sider>
 
-        <Layout className={styles.contentWrap}>
-          <Header className={styles.conHeader} />
-          <Content className={styles.conDom}>
-            {routes.map((item, i) => (
-              <Route key={i.toString()} path={item.path} component={item.component} exact />
-            ))}
-          </Content>
-          <Footer style={{ textAlign: 'center' }}>SummerSnake</Footer>
+            <Layout className={styles.contentWrap}>
+                <Header className={styles.conHeader} />
+                <Content className={styles.conDom}>
+                    {routes.map((item, i) => (
+                        <Route key={i.toString()} path={item.path} component={item.component} exact />
+                    ))}
+                </Content>
+                <Footer style={{ textAlign: 'center' }}>SummerSnake</Footer>
+            </Layout>
         </Layout>
-      </Layout>
     );
-  }
 }
