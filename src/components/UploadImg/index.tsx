267a9c1484f33b76f5558b909fe1a255e299e426
summersnake <33661226lll@163.com>
2019-08-02 11:20:07 +0800

    【新增】上传图片组件

---
+++ src/components/UploadImg/index.tsx
@@ -0,0 +1,70 @@
+import React, { useState, useEffect } from 'react';
+import { Upload, Modal, Icon } from 'antd';
+
+interface InitProp {
+  uploadCall: any;
+}
+function UploadImg(props: InitProp) {
+  const [previewVisible, setPreviewVisible] = useState<boolean>(false);
+
+  const [previewImage, setPreviewImage] = useState<string>('');
+
+  const [fileList, setFileList] = useState([]);
+
+  /**
+   * 预览图片
+   */
+  function handlePreview(file) {
+    const imgUrl = file.url || file.thumbUrl;
+    setPreviewImage(imgUrl);
+    setPreviewVisible(true);
+  }
+
+  /**
+   * 取消预览图片
+   */
+  function handleCancel() {
+    setPreviewVisible(false);
+  }
+
+  /**
+   * 上传图片
+   */
+  function handleChange({ fileList }) {
+    let imgUrl = '';
+    fileList.forEach(file => {
+      if (typeof file.response !== 'undefined') {
+        imgUrl = `${imgUrl + file.response.data}#`;
+      } else {
+        imgUrl = `${imgUrl + file.name}#`;
+      }
+    });
+    props.uploadCall(imgUrl);
+    setFileList(fileList);
+  }
+
+  const uploadButton = (
+    <div>
+      <Icon type="plus" />
+      <div className="ant-upload-text">Upload</div>
+    </div>
+  );
+
+  return (
+    <div className="clearfix">
+      <Upload
+        action=""
+        listType="picture-card"
+        fileList={fileList}
+        onPreview={handlePreview}
+        onChange={handleChange}
+      >
+        {fileList.length >= 5 ? null : uploadButton}
+      </Upload>
+      <Modal maskClosable={false} visible={previewVisible} footer={null} onCancel={handleCancel}>
+        <img alt="example" style={{ width: '100%' }} src={previewImage} />
+      </Modal>
+    </div>
+  );
+}
+export default UploadImg;
