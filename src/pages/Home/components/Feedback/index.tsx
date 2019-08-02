16f99636b31b31d915d52cf3250ee2a65884f127
summersnake <33661226lll@163.com>
2019-08-02 11:20:45 +0800

    【新增】首页--Feedback--上传图片

--- src/pages/Home/components/Feedback/index.tsx
+++ src/pages/Home/components/Feedback/index.tsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import { Input, Button } from 'antd';
+import UploadImg from '@components/UploadImg';
 import styles from './index.less';
 
 function Feedback() {
   let areaVal = '';
+  let imgUrl = '';
 
   /**
    * textArea 内容更新
    */
   function handleAreaChange(e) {
     areaVal = e.target.value;
+  }
+
+  /**
+   * 上传图片回调
+   */
+  function uploadCall(url): void {
+    imgUrl = url;
   }
 
   return (
@@ -22,9 +31,15 @@ function Feedback() {
           <span>周一至周五 9:00 - 18:00</span>
         </p>
       </div>
+
       <div>
         <Input.TextArea rows={6} placeholder="请输入您的意见和建议" onChange={handleAreaChange} />
       </div>
+
+      <div className={styles.uploadDom}>
+        <UploadImg fileList={imgUrl} uploadCall={uploadCall} />
+      </div>
+
       <div className={styles.footerBtn}>
         <Button type="primary">提交</Button>
       </div>
