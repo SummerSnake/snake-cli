9a8fb22b7e7e239de49e3181384c9bbc3cbd2812
SummerSnake <33661226lll@163.com>
2019-09-17 11:39:59 +0800

    feat: 新增 table 封装 -- OperationBtn 组件

---
+++ src/components/TableRigger/OperationBtn/index.tsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import { Popconfirm, Button } from 'antd';
+import { connect } from 'react-redux';
+import { verVal } from '@utils/util';
+import styles from './index.less';
+
+interface InitProp {
+  tableRigger?: {
+    query?: any;
+    queryShow?: any;
+    pagination?: any;
+    orders?: any;
+  };
+  dispatch?: any;
+  mode?: number;
+  btnType?: string;
+  isBtn?: boolean;
+  btnLoading?: boolean;
+  title?: string;
+  reminder?: string;
+  onClick?: () => void;
+}
+function OperationBtn(props: InitProp = {}) {
+
+  const handleClick = async () => {
+    const { onClick, tableRigger = {}, dispatch, mode } = props;
+    const { query, queryShow, pagination, orders } = tableRigger;
+
+    if (verVal(onClick)) {
+      await onClick();
+      // mode 是否携带筛选条件刷新
+      if (mode === 0) {
+        const code = Math.round(Math.random() * 999999999);
+        dispatch({
+          type: 'tableRigger/fetch',
+          payload: { query, queryShow, pagination, orders, code },
+        });
+      } else if (mode === 1) {
+        dispatch({
+          type: 'tableRigger/init',
+        });
+      }
+    }
+  };
+
+  const { isBtn, btnType, btnLoading, title, reminder } = props;
+  return (
+    <span>
+      {isBtn ? (
+        <Button
+          type={btnType === 'submit' ? 'primary' : 'default'}
+          className={btnType === 'submit' ? styles.submitBtn : styles.cancelBtn}
+          loading={btnLoading}
+          onClick={handleClick}
+        >
+          {title}
+        </Button>
+      ) : (
+        <span>
+          {reminder ? (
+            <Popconfirm title={reminder} onConfirm={handleClick}>
+              <a>{title}</a>
+            </Popconfirm>
+          ) : (
+            <a onClick={handleClick}>{title}</a>
+          )}
+        </span>
+      )}
+    </span>
+  );
+}
+
+export default connect(({ tableRigger }: any) => ({ tableRigger }))(OperationBtn);
--- src/components/TableRigger/index.ts
+++ src/components/TableRigger/index.ts
@@ -3,5 +3,6 @@ import Statistics from '@components/TableRigger/Statistics';
 import FilterTags from '@components/TableRigger/FilterTags';
 import Search from '@components/TableRigger/Search';
 import FormModal from '@components/TableRigger/FormModal';
+import OperationBtn from '@components/TableRigger/OperationBtn';
 
-export { CommonTable, Statistics, FilterTags, Search, FormModal };
+export { CommonTable, Statistics, FilterTags, Search, FormModal, OperationBtn };
