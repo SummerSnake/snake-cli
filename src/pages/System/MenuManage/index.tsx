3b16a99c96944d9850136b30e899089bcae9a794
liyan <33661226lll@163.com>
2020-03-19 17:28:45 +0800

    feat: 菜单管理详情

--- src/pages/System/MenuManage/index.tsx
+++ src/pages/System/MenuManage/index.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { WindowsOutlined, TableOutlined, SettingOutlined, FolderOutlined } from '@ant-design/icons';
-import { Spin, Tree, Tag } from 'antd';
+import { Spin, Tree, Tag, Popconfirm } from 'antd';
 import { getRequest } from '@services/api';
 import styles from './index.less';
 import '../../../../mock/menuManageApi';
@@ -15,6 +15,7 @@ function MenuManage() {
   const [loading, setLoading] = useState<boolean>(false);
   const [isTagSelect, setIsTagSelect] = useState<number>(0);
   const [menuList, setMenuList] = useState<any[]>([]);
+  const [menuItemInfo, setMenuItemInfo] = useState<any>({});
 
   const recursion = (menuList = [], index: number) => {
     const arr = [];
@@ -25,7 +26,10 @@ function MenuManage() {
             title={
               <Tag
                 className={isTagSelect === item.id ? styles.treeNodeActive : styles.treeNode}
-                onClick={() => setIsTagSelect(item.id)}
+                onClick={() => {
+                  setIsTagSelect(item.id);
+                  setMenuItemInfo(item);
+                }}
               >
                 {Icons[item.menuImg]} &nbsp;&nbsp;{item.menuName}
               </Tag>
@@ -63,7 +67,10 @@ function MenuManage() {
                 title={
                   <Tag
                     className={isTagSelect === 0 ? styles.treeNodeActive : styles.treeNode}
-                    onClick={() => setIsTagSelect(0)}
+                    onClick={() => {
+                      setIsTagSelect(0);
+                      setMenuItemInfo({});
+                    }}
                   >
                     <FolderOutlined />
                     &nbsp;&nbsp;菜单结构
@@ -76,6 +83,38 @@ function MenuManage() {
             </Tree>
           </div>
         </Spin>
+
+        <div className={styles.menuRight} style={{ display: !!menuItemInfo.id ? 'block' : 'none' }}>
+          <div>
+            <div>
+              <span className={styles.btnDom}>添加下级菜单</span>
+              <span
+                className={styles.btnDom}
+                style={{ display: !!menuItemInfo.id ? 'inline-block' : 'none' }}
+              >
+                修改菜单
+              </span>
+              <Popconfirm title="确认删除吗">
+                <span
+                  className={styles.btnDom}
+                  style={{ display: !!menuItemInfo.id ? 'inline-block' : 'none' }}
+                >
+                  删除菜单
+                </span>
+              </Popconfirm>
+            </div>
+            <div className={styles.tagsWrap}>
+              <Tag>菜单名称: {menuItemInfo.menuName}</Tag>
+              <Tag>菜单标识: {menuItemInfo.menuCode}</Tag>
+              <Tag>菜单类别: {menuItemInfo.menuType}</Tag>
+              <Tag>菜单图标: {menuItemInfo.menuImg}</Tag>
+              <Tag>菜单地址: {menuItemInfo.menuUrl}</Tag>
+              <Tag>菜单顺序: {menuItemInfo.sort}</Tag>
+              <Tag>父级名称: {menuItemInfo.parentName}</Tag>
+              <Tag>备注: {menuItemInfo.remarks}</Tag>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
